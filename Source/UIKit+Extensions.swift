1aed9fe425998df1f9fce1bf9b86bf27db32ea55
Zef Houssney <zef@madebykiwi.com>
2019-04-04 21:41:51 -0600

    Implement new UIKit extensions. autoLayout var, insertSubview variants. Add some inline code docs.

--- Source/UIKit+Extensions.swift
+++ Source/UIKit+Extensions.swift
@@ -9,32 +9,103 @@
 import UIKit
 
 extension UIView {
-    func addSubview(_ view: UIView, autolayout: Bool) {
+    var autoLayout: Bool {
+        get { return translatesAutoresizingMaskIntoConstraints }
+        set { translatesAutoresizingMaskIntoConstraints = !newValue }
+    }
+
+    func addSubview(_ view: UIView, autoLayout: Bool) {
         addSubview(view)
-        view.translatesAutoresizingMaskIntoConstraints = !autolayout
+        self.autoLayout = autoLayout
     }
 
-    @discardableResult
-    func addSubview(_ view: UIView, layout layouts: Layout...) -> [NSLayoutConstraint] {
-        return addSubview(view, layout: layouts)
+    func insertSubview(_ view: UIView, at index: Int, autoLayout: Bool) {
+        insertSubview(view, at: index)
+        self.autoLayout = autoLayout
     }
 
-    @discardableResult
-    func addSubview(_ view: UIView, layout layouts: [Layout]) -> [NSLayoutConstraint] {
-        addSubview(view, autolayout: true)
-        return view.apply(layout: layouts, with: self)
+    func insertSubview(_ view: UIView, aboveSubview siblingSubview: UIView, autoLayout: Bool) {
+        insertSubview(view, aboveSubview: siblingSubview)
+        self.autoLayout = autoLayout
     }
 
+    func insertSubview(_ view: UIView, belowSubview siblingSubview: UIView, autoLayout: Bool) {
+        insertSubview(view,  belowSubview: siblingSubview)
+        self.autoLayout = autoLayout
+    }
+
+
+    // MARK: Apply layouts after views have been added
+    //
+    // The following functions in this extension come in pairs:
+    //
+    // - The `Layout...` variant uses a Variadic Parameter to allow you to pass in
+    //   a list of layouts without providing the square brackets for an array.
+    //
+    // - The `[Layout]` variant allows you to store and pass around an actual Array<Layout>,
+    //   enabling more powerful and dynamic usage, if needed.
+    //   A good use of this would be to store a commonly used or complex layout configuration
+    //   and apply it by passing in an array, instead of duplicating the configuration inline
+    //
     @discardableResult
     func apply(layout layouts: Layout..., with view: UIView) -> [NSLayoutConstraint] {
         return apply(layout: layouts, with: view)
     }
-
     @discardableResult
     func apply(layout layouts: [Layout], with view: UIView) -> [NSLayoutConstraint] {
         return layouts.flatMap { layout in
             layout.apply(to: self, with: view)
         }
+    }
+
+    // MARK: Applying layouts while adding
+    //
+    // These functions mirror UIView's four functions for adding subviews,
+    // allowing you to always apply your layouts at the time of view insertion:
+    //
+    // - addSubview(myView, layout: .pin(10))
+    // - insertSubview(myView, at: 0, layout: .fill)
+    // - insertSubview(myView, aboveSubview: otherView, layout: .centerX, .width(60), .height(100))
+    // - insertSubview(myView, belowSubview: otherView, layout: .center(y: -40), .fillX)
+    //
+    @discardableResult
+    func addSubview(_ view: UIView, layout layouts: Layout...) -> [NSLayoutConstraint] {
+        return addSubview(view, layout: layouts)
+    }
+    @discardableResult
+    func addSubview(_ view: UIView, layout layouts: [Layout]) -> [NSLayoutConstraint] {
+        addSubview(view, autoLayout: true)
+        return view.apply(layout: layouts, with: self)
+    }
+
+    @discardableResult
+    func insertSubview(_ view: UIView, at index: Int, layout layouts: Layout...) -> [NSLayoutConstraint] {
+        return insertSubview(view, at: index, layout: layouts)
+    }
+    @discardableResult
+    func insertSubview(_ view: UIView, at index: Int, layout layouts: [Layout]) -> [NSLayoutConstraint] {
+        insertSubview(view, at: index, autoLayout: true)
+        return view.apply(layout: layouts, with: self)
+    }
+
+    @discardableResult
+    func insertSubview(_ view: UIView, aboveSubview siblingSubview: UIView, layout layouts: Layout...) -> [NSLayoutConstraint] {
+        return insertSubview(view, aboveSubview: siblingSubview, layout: layouts)
+    }
+    @discardableResult
+    func insertSubview(_ view: UIView, aboveSubview siblingSubview: UIView, layout layouts: [Layout]) -> [NSLayoutConstraint] {
+        insertSubview(view, aboveSubview: siblingSubview, autoLayout: true)
+        return view.apply(layout: layouts, with: self)
+    }
+
+    @discardableResult
+    func insertSubview(_ view: UIView, belowSubview siblingSubview: UIView, layout layouts: Layout...) -> [NSLayoutConstraint] {
+        return insertSubview(view, belowSubview: siblingSubview, layout: layouts)
+    }
+    @discardableResult
+    func insertSubview(_ view: UIView, belowSubview siblingSubview: UIView, layout layouts: [Layout]) -> [NSLayoutConstraint] {
+        insertSubview(view, belowSubview: siblingSubview, autoLayout: true)
+        return view.apply(layout: layouts, with: self)
     }
 }
 
